4ae2d045a0c70e79ba26a0c6cb71c201262a23d3
tiago <tiago.zdo@gmail.com>
2024-05-27 09:58:22 -0300

    Lista las tareas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,32 @@
+import { prisma } from "@/libs/prisma";
 
-function HomePage() {
+async function loadTasks() {
+  // via HTTP
+  // const res = await fetch("http://localhost:3000/api/tasks");
+  // const data = await res.json();
+  // console.log(data);
+
+  // via Prisma
+  const tasks = await prisma.task.findMany();
+  return tasks;
+}
+
+async function HomePage() {
+  const tasks = await loadTasks();
   return (
-    <div>Tareas</div>
+    <section className="container mx-auto">
+      <div className="grid grid-cols-3 gap-3 mt-10">
+        {
+          tasks.map((task) => (
+            <div key={task.id} className="bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer">
+              <h3 className="font-bold text-2xl mb-2">{task.title}</h3>
+              <p>{task.description}</p>
+              <p>{new Date(task.createdAt).toLocaleDateString()}</p>
+            </div>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
