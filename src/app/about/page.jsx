0ed650f601cf8a01386bd256c14b25a3886974e2
tiago <tiago.zdo@gmail.com>
2024-05-29 10:55:10 -0300

    Navegación

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,10 @@
 
 function AboutPage() {
     return (
-        <div>AboutPage</div>
+        <section className="container mx-auto">
+            <h3 className="text-4xl font-bold my-5">About</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur doloremque cum quam similique perferendis, culpa inventore excepturi nam maiores quasi architecto est, voluptates id exercitationem nemo officiis rerum impedit distinctio odio eius repellendus. Amet non atque, inventore voluptatibus totam ad minima labore doloribus nisi nam neque cupiditate? Hic tenetur distinctio quas dolorem, adipisci corporis sed eius in doloremque dolorum nesciunt animi fugiat quasi facere cupiditate quidem blanditiis esse placeat! Suscipit in, repellat impedit quia reprehenderit eos quibusdam officiis perspiciatis fugiat laborum. Distinctio debitis facilis quisquam odio voluptate dolorem numquam tempora est aut hic, eius eos qui laudantium, cumque neque harum!</p>
+        </section>
     )
 }
 
---
+++ src/app/not-found.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+function NotFound() {
+  return (
+    <section className="flex h-[calc(100vh-7rem)] justify-center items-center">
+      <div>
+        <h1 className="text-4xl font-bold">Not Found</h1>
+        <Link href="/" className="text-center text-slate-400 text-2xl mt-5">
+          Volver al inicio
+        </Link>
+      </div>
+    </section>
+  )
+}
+
+export default NotFound
